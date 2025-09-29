8ae18d965c2be42fceb9b14174089da9dfbc124e
Lancerfx <yt0328575@gmail.com>
2025-09-29 22:27:54 +0545

    day 2

--- hello.js
+++ hello.js
@@ -1,5 +1,9 @@
-function hello(){
-  return "Helloworld!"
+function Outside(){
+  document.getElementById("External").innerHTML = "Congrulation External Js is success.";
 }
 
-//hello();
+  function Change(){
+    document.getElementById("Function").innerHTML = "This paragraph is changed Thanku for co-operation.";
+  }
+
+  
