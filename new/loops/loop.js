e428ed9bb884b9ba2ff484aba0da39aff237d25c
Lancerfx <yt0328575@gmail.com>
2025-10-19 07:44:38 +0545

    for loop

---
+++ new/loops/loop.js
@@ -0,0 +1,99 @@
+console.log("I am learning loops.")
+// let a = 1;
+// for(let i = 0; i < 100; i++){
+//   console.log("Number is: ",a+i);
+// }
+
+// Print numbers from 1 to 10 using a for loop.
+
+// Print numbers from 10 down to 1 using a for loop.
+
+// Print only even numbers between 1 and 20.
+
+// Print only odd numbers between 1 and 20.
+
+// Print multiples of 5 up to 50 (like 5, 10, 15…).
+
+// Add all numbers from 1 to 10 and print the total.
+
+// Print numbers from 50 to 100.
+
+// Print numbers between 0 and 30, but skip numbers less than 10.
+
+// Print “JavaScript is fun!” 5 times using a loop.
+
+// Use a variable start = 3 and print the next 10 numbers after it.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Print numbers from 1 to 10 using a for loop.
+
+for(let i = 1; i <= 10; i++){
+  console.log("The number is: ",i);
+}
+
+
+// Print numbers from 10 down to 1 using a for loop.
+
+for(let i = 10; i >= 1; i--){
+  console.log("The number is: ",i)
+}
+
+
+// Print only even numbers between 1 and 20.
+
+for(let i = 20; i/2; i--){
+  console.log("The number is even: ",i)
+}
+
+// Print only odd numbers between 1 and 20.
+
+for(let i= 1; i/2; i++){
+  console.log("The number is odd: ",i);
+}
+
+
+// Print multiples of 5 up to 50 (like 5, 10, 15…).
+for(let i = 5; i<= 50; i++){
+console.log("multiplication of 5: ",i);
+}
+
+
+
+// Print numbers from 50 to 100.
+
+for(let i =50; i <= 10; i++){
+  console.log("number from 50 to 100: ", i);
+}
+
+// Print numbers between 0 and 30, but skip numbers less than 10.
+
+for(let i = 10; i <30; i++){
+  console.log("numbers between 0 and 30, but skip numbers less than 10: ",i);
+}
+
+// Print “JavaScript is fun!” 5 times using a loop.
+
+for(let i = 1; i <= 5; i++){
+  console.log(i,") JavaScript is fun!");
+}
+
+
+// Use a variable start = 3 and print the next 10 numbers after it.
+
+for(let start = 3; start <= 13; i++){
+  console.log("variable start = 3 and print the next 10 numbers after it: ",i)
+}
